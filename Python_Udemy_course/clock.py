e391042dcf5b2d0d58c8d05316affc4cb45e0b9f
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-03 14:35:24 +0100

    modified clock script

--- Python_Udemy_course/clock.py
+++ Python_Udemy_course/clock.py
@@ -6,19 +6,19 @@ from tkinter import Label, Tk
 
 # Window config for clock
 window = Tk()
-window.title("")
-window.geometry("300x80")
-window.configure(bg="blue")
+window.title("Digital Clock")
+window.geometry("400x100")
+window.configure(bg="black")
 window.resizable(False,False)
 
 # Label config
-clock_label = Label(window, bg="blue", fg="yellow", font=("Times", 30, "bold"), relief="flat")
-clock_label.place(x = 20, y = 20)
+clock_label = Label(window, bg="black", fg="cyan", font=("Helvetica", 48, "bold"), relief="flat")
+clock_label.pack(pady=20)
 
 def updating_label():
-    current_time = strftime('%H, %M, %S')
+    current_time = strftime('%H:%M:%S')
     clock_label.configure(text=current_time)
-    clock_label.after(80, updating_label)
+    clock_label.after(1000, updating_label)
 
 updating_label()
 window.mainloop()
