eb60e725bd63d3a2223d57e24eed67b54bad0bad
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-04 22:03:07 +0100

    modified goslate translator

--- Python_Udemy_course/goslate_1.py
+++ Python_Udemy_course/goslate_1.py
@@ -3,8 +3,13 @@
 
 import goslate
 
-print("Hi, I'm a translator. I translate English to French")
-inserted_text = input("Input the text you want to translate: ")
-new_gs = goslate.Goslate()
-trans_late = new_gs.translate(inserted_text, 'fr')
-print(trans_late + "\n")
+
+def translator():
+    print("Hi, I'm a translator. I translate English to French")
+    inserted_text = input("Input the text you want to translate: ")
+    new_gs = goslate.Goslate()
+    trans_late = new_gs.translate(inserted_text, 'fr')
+    print(trans_late + "\n")
+
+
+translator()
