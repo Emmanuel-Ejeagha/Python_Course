55b54b1965554020a209b16e06c95a61bf8dff76
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-04-10 15:56:30 +0100

    code playground

---
+++ 100DaysOfCodeWithHarry/main_rough.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# This script is a  playground of codes
+a = 5
+b = 4
+
+print("The value of", a, "+", b, "is", a + b)
+print("The value of", a, "*", b, "is", a * b)
+str1 = "Hello world090"
+print(str1.isalnum())
+
+# strings are immutable
+a = "!Emma!! !!!!!!!! Emma"
+print(len(a))
+
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Emma", "Emmanuel"))
+a = "Python is an Interpreted Language"
+print(a.istitle())
+print(a.startswith("Python"))
+
+print(a.swapcase())
+a = "python is a programming language"
+print(a.title())
