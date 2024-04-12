ae72be25cea6799021f69da8d98cad4f8a2a366b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-04-12 22:45:55 +0100

    time file

---
+++ 100DaysOfCodeWithHarry/time1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import time
+import os
+
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+os.system("python --version")#!/usr/bin/python3
+import time
+import os
+
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+os.system("python3 --version")
