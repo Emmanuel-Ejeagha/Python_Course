466f5d3d31098796c51fd3e4285f0f273f465ae4
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-04-15 12:43:45 +0100

    updated: sets file

--- 100DaysOfCodeWithHarry/sets.py
+++ 100DaysOfCodeWithHarry/sets.py
@@ -19,4 +19,15 @@ states1 = {'Alaska', 'Miami', 'Atlanta', 'Texas'}
 states2 = {'WDC', 'Chicago', 'Arizona', 'Miami', 'Alaska'}
 x = states1.intersection(states2)
 print(x)
+y = states1.difference(states2)
+print(y)
+z = states1.isdisjoint(states2)     # return True if the two sets are disjoint
+print(z)
+print(states1.issuperset(states2))
+states1.add("Las-vegas")
+states1.add("Los Angeles")
 
+print(states1)
+item = states1.pop()
+print(item)
+print(states1)
