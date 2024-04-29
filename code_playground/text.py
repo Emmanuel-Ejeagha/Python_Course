80c1bd94e45850d8027afcc85409e1223272b13f
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-04-29 23:13:36 +0100

    practicals

---
+++ code_playground/text.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+words = ['cat', 'dog', 'dragon', 'horse']
+
+
+for word in words:
+    w = len(word)
+    print(f"{word} contains {w} alphabets")
+
+names = ['Kingsley', 'Mercy', 'Emeka', 'Henry']
+
+for name in range(len(names)):
+    print(name, names[name]) 
