a0732486051a0266faf102fc955ed7876403c1e9
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-01 16:41:27 +0100

    Added: index.js file

---
+++ JavaScript_Course/01-Fundamentals-part-1/script.js
@@ -0,0 +1,22 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonah');
+console.log(23);
+
+
+let $name = 'Kyrian';
+console.log(firstName)
+console.log($name)
+// This is a single single line comment
+/* 
+This is a
+Multi
+Line
+comment
+*/
+
+// Boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+console.log(typeof true)
