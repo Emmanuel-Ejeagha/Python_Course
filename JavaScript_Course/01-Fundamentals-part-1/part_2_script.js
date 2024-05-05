144f0cbc96b7fd2bd3caa4c5685ce361fcd5c2ef
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-05 22:01:14 +0100

    add some functions

--- JavaScript_Course/01-Fundamentals-part-1/part_2_script.js
+++ JavaScript_Course/01-Fundamentals-part-1/part_2_script.js
@@ -45,7 +45,7 @@ console.log(age2)
 const calcAge3 = birthYear => 2024 - birthYear;
 const age3 = calcAge3(1996)
 console.log(age3)
-*/
+
 
 // Arrow function
 const yearUntilRetirement = (birthYear, firstName) => {
@@ -56,3 +56,50 @@ const yearUntilRetirement = (birthYear, firstName) => {
 let retire1 = yearUntilRetirement(1991, 'James')
 console.log(retire1)
 console.log(yearUntilRetirement(1980, 'Bob'))
+const calcAge  = function (birthYear) {
+    return 2024 - birthYear;
+}
+const year = [1990, 1997, 2002, 1994];
+const age1 = calcAge(year[0]);
+const age2 = calcAge(year[1]);
+const age3 = calcAge(year[year.length - 1]);
+console.log(age1, age2, age3);
+
+const years = new Array(1991, 1992, 1993, 1994, 1995)
+console.log(years[0])
+const ages = [calcAge(year[0]), calcAge(year[1]),
+calcAge(year[year.length - 1])];
+console.log(ages);
+*/
+
+// Arrays
+const friends = ['Ken', 'steven', 'Miracle', 'Sharom']
+console.log(friends)
+
+
+console.log(friends.length)
+console.log(friends[friends.length - 1]);
+
+// Add elements
+friends.push = ('Seyi'); // add elements at the end of the array
+console.log(friends);
+
+friends.unshift('John') // add elements at the beginning of the array
+console.log(friends);
+
+// Remove elements
+let popped = friends.pop()
+console.log(friends);
+console.log(popped);
+
+friends.shift()
+console.log(friends);
+
+console.log(friends.indexOf('steven')); // displays the index num of an element
+console.log(friends.includes('Bob'));
+console.log(friends.includes('Miracle'))
+console.log(friends.indexOf('Miracle'))
+
+if (friends.includes('Miracle')) {
+    console.log(`You have a friend called ${friends[2]}.`)
+}
