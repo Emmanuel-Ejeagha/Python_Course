38214674bcb66cfdc33910450532b5832487bfbe
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-11 23:20:20 +0100

    added: classList

---
+++ JavaScript_Course/modal/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+const closelModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closelModal);
+overlay.addEventListener('click', closelModal);
