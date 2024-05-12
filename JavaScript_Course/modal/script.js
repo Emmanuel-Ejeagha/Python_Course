941131a9083f002a87d00a6ea9865f29984184ae
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-12 22:09:00 +0100

    added: Eventhandler for key press

--- JavaScript_Course/modal/script.js
+++ JavaScript_Course/modal/script.js
@@ -1,15 +1,18 @@
 'use strict';
 
+// variable declaration
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
+// This function hides the modal class
 const closelModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
+// This function displays the modal class
 const showModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -18,5 +21,13 @@ const showModal = function () {
 for (let i = 0; i < btnShowModal.length; i++)
   btnShowModal[i].addEventListener('click', showModal);
 
+// Hides th modal class by clicking on the window
 btnCloseModal.addEventListener('click', closelModal);
 overlay.addEventListener('click', closelModal);
+
+// closes the window when ESC is pressed
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closelModal();
+});
