c64b6f1dfc3bbf177830836f5c74c908b2598b13
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-15 16:51:02 +0100

    Added: comments

--- flask_projects/website_flask/main.py
+++ flask_projects/website_flask/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
+# Import the function create_app from the website package
 from website import create_app
 
+# Create the Flask application using the create_app function
 app = create_app()
 
-
+# Check if the script is being run directly
 if __name__ == "__main__":
+    # Run the Flask application in debug mode
     app.run(debug=True)
