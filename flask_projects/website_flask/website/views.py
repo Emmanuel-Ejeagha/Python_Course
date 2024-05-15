6373272b755b0d02ce7201496a5d2b8a8fa352ea
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-15 16:49:18 +0100

    Added: comments

--- flask_projects/website_flask/website/views.py
+++ flask_projects/website_flask/website/views.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
+
+# Import necessary modules
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
-from . import db
+from .models import Note  # Import the Note model
+from . import db  # Import the database object
 import json
 
+# Create a Blueprint object for views
 views = Blueprint('views', __name__)
 
-
+# Route for the home page
 @views.route('/', methods=['GET', 'POST'])
-@login_required
+@login_required  # Require login to access this route
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+    if request.method == 'POST':  # If the request method is POST (i.e., submitting a form)
+        note = request.form.get('note')  # Get the note from the HTML form
 
         if len(note) < 1:
-            flash('Note is too short!', category='error') 
+            flash('Note is too short!', category='error')  # Display an error message if the note is too short
         else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
+            # Create a new Note object and add it to the database
+            new_note = Note(data=note, user_id=current_user.id)  # Create a new note object with current user's ID
+            db.session.add(new_note)  # Add the new note to the database session
+            db.session.commit()  # Commit the changes to the database
+            flash('Note added!', category='success')  # Display a success message
 
+    # Render the home template with the current user
     return render_template("home.html", user=current_user)
 
-
+# Route for deleting a note
 @views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
+def delete_note():
+    note = json.loads(request.data)  # Load the JSON data sent from the frontend
+    noteId = note['noteId']  # Extract the note ID from the JSON data
+    note = Note.query.get(noteId)  # Query the database for the note with the given ID
     if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
+        if note.user_id == current_user.id:  # Check if the note belongs to the current user
+            db.session.delete(note)  # Delete the note from the database
+            db.session.commit()  # Commit the changes to the database
 
-    return jsonify({})
+    return jsonify({})  # Return an empty JSON response
