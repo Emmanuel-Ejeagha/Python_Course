6ecb054089e72a4a80543bcc3ad9e1b85d3c4af8
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-15 16:48:50 +0100

    Added: comments

--- flask_projects/website_flask/website/models.py
+++ flask_projects/website_flask/website/models.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-from website import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
 
+# Import necessary modules
+from website import db  # Import the database object from the website package
+from flask_login import UserMixin  # Import UserMixin for user management
+from sqlalchemy.sql import func  # Import func for database functions
+
+# Define the Note model for storing user notes
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    
+    id = db.Column(db.Integer, primary_key=True)  # Unique identifier for each note
+    data = db.Column(db.String(10000))  # Actual content of the note, limited to 10000 characters
+    date = db.Column(db.DateTime(timezone=True), default=func.now())  # Date and time of note creation
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))  # Foreign key linking to the User model
 
+# Define the User model for storing user information
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    id = db.Column(db.Integer, primary_key=True)  # Unique identifier for each user
+    email = db.Column(db.String(150), unique=True)  # Email address of the user, must be unique
+    password = db.Column(db.String(150))  # Hashed password of the user
+    first_name = db.Column(db.String(150))  # First name of the user
+    notes = db.relationship('Note')  # Relationship between users and their notes
