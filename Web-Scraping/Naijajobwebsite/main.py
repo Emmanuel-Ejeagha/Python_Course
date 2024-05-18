036504603b51cd5922add4f4fefbc36abf61a48f
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-18 22:55:15 +0100

    started the project

--- Web-Scraping/Naijajobwebsite/main.py
+++ Web-Scraping/Naijajobwebsite/main.py
@@ -3,3 +3,9 @@ import requests
 
 html_text = requests.get('https://www.myjobmag.com/').text
 soup = BeautifulSoup('html_text', 'lxml')
+
+company_name = soup.find('li', class_="job-desc")
+# date_posted = soup.find('div', class_='read-date-sec-li')
+# job_info = soup.find(class_='job-key-info').text
+# job_details = soup.find(class_='job-details')
+print(company_name)
