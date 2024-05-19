a11ee981fe78eb006e021cf5de572ce279dc7a59
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-19 22:33:51 +0100

    added: job description

--- Web-Scraping/Naijajobwebsite/main.py
+++ Web-Scraping/Naijajobwebsite/main.py
@@ -2,10 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://www.myjobmag.com/').text
-soup = BeautifulSoup('html_text', 'lxml')
+soup = BeautifulSoup(html_text, 'lxml')
 
-company_name = soup.find('li', class_="job-desc")
-# date_posted = soup.find('div', class_='read-date-sec-li')
-# job_info = soup.find(class_='job-key-info').text
-# job_details = soup.find(class_='job-details')
-print(company_name)
+jobs = soup.find_all('li', class_="job-list-li")
+
+for job in jobs:
+    company_name = job.find('h2').text if job.find('h2') else 'N/A'
+    job_desc = job.find('li', class_='job-desc').text if job.find('li', class_='job-desc') else 'N/A'
+    job_date = job.find('li', id='job-date').text if job.find('li', id='job-date') else 'N/A'
+    job_desc = job.find('li', class_='mag_b')
+    print(f'''
+    Company name: {company_name}
+    Company Info: {job_desc}
+    Date posted: {job_date}
+    ''')
