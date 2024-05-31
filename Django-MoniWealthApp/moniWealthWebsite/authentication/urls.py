e6f9f11fc31062a48848e22638fab5465461732d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-31 16:54:46 +0100

    added: validate-email and username

--- Django-MoniWealthApp/moniWealthWebsite/authentication/urls.py
+++ Django-MoniWealthApp/moniWealthWebsite/authentication/urls.py
@@ -1,7 +1,10 @@
-from .views import RegistrationVeiw
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('register', RegistrationVeiw.as_view(), name='register')
+    path('register', RegistrationView.as_view(), name="register"),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate-email')
 ]
