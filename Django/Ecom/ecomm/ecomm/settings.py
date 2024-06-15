b1860fd2ca0071aa7990362039e9703138d0027a
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-15 23:28:08 +0100

    Added media

--- Django/Ecom/ecomm/ecomm/settings.py
+++ Django/Ecom/ecomm/ecomm/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ecomm/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 
 # Default primary key field type
