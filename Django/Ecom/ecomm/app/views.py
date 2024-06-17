07a42c71250b1d8a03d9e9af8bfc9c93fcd0741a
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-17 22:51:04 +0100

    modified

--- Django/Ecom/ecomm/app/views.py
+++ Django/Ecom/ecomm/app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from urllib import request
 from django.views import View
+from . models import Product
+from django.db.models import Count
+
 
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
 
 class CategoryView(View):
-    def get(self, request):
-        return render(request, "app/category.html")
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request, "app/category.html", locals())
+
+class ProductDetail(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "app/productdetail.html", locals())
