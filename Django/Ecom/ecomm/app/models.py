94454a69f5d538700271ce534cb5a9ca478b6d5d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-19 19:59:54 +0100

    Added: login and signup form

--- Django/Ecom/ecomm/app/models.py
+++ Django/Ecom/ecomm/app/models.py
@@ -1,7 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+# State Choices
+STATE_CHOICES = (
+    ('AB', 'Abia'),
+    ('AD', 'Adamawa'),
+    ('AK', 'Akwa Ibom'),
+    ('AN', 'Anambra'),
+    ('BA', 'Bauchi'),
+    ('BY', 'Bayelsa'),
+    ('BE', 'Benue'),
+    ('BO', 'Borno'),
+    ('CR', 'Cross River'),
+    ('DE', 'Delta'),
+    ('EB', 'Ebonyi'),
+    ('ED', 'Edo'),
+    ('EK', 'Ekiti'),
+    ('EN', 'Enugu'),
+    ('GO', 'Gombe'),
+    ('IM', 'Imo'),
+    ('JI', 'Jigawa'),
+    ('KD', 'Kaduna'),
+    ('KN', 'Kano'),
+    ('KT', 'Katsina'),
+    ('KE', 'Kebbi'),
+    ('KO', 'Kogi'),
+    ('KW', 'Kwara'),
+    ('LA', 'Lagos'),
+    ('NA', 'Nasarawa'),
+    ('NI', 'Niger'),
+    ('OG', 'Ogun'),
+    ('ON', 'Ondo'),
+    ('OS', 'Osun'),
+    ('OY', 'Oyo'),
+    ('PL', 'Plateau'),
+    ('RI', 'Rivers'),
+    ('SO', 'Sokoto'),
+    ('TA', 'Taraba'),
+    ('YO', 'Yobe'),
+    ('ZA', 'Zamfara'),
+    ('FC', 'Federal Capital Territory (FCT)'),
+)
+
+
+
+# Product Category
 CATEGORY_CHOICES = (
     ('PH', 'Phone'),
     ('AP', 'Airpod'),
@@ -23,3 +68,14 @@ class Product(models.Model):
     product_image = models.ImageField(upload_to='product')
     def __str__(self):
         return self.title
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    def __str__(self):
+        return self.name
