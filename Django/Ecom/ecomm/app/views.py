1baa107fd7753fbf89a5ad54ac5fdcaee6240651
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-28 19:45:15 +0100

    Added: Password setup

--- Django/Ecom/ecomm/app/views.py
+++ Django/Ecom/ecomm/app/views.py
@@ -1,30 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from . models import Customer, Product
+from .models import Customer, Product
 from django.db.models import Count
-from . forms import CustomerRegistrationForm, CustomerProfileForm
+from .forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 
-# Create your views here.
 def home(request):
     return render(request, 'app/home.html')
-
 
 def about(request):
     return render(request, 'app/about.html')
 
-
 def contact(request):
     return render(request, 'app/contact.html')
-
-
 
 class CategoryView(View):
     def get(self, request, val):
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title')
         return render(request, "app/category.html", locals())
-    
+
 class CategoryTitle(View):
     def get(self, request, val):
         product = Product.objects.filter(title=val)
@@ -36,26 +31,25 @@ class ProductDetail(View):
         product = Product.objects.get(pk=pk)
         return render(request, "app/productdetail.html", locals())
 
-
 class CustomerRegistrationView(View):
     def get(self, request):
         form = CustomerRegistrationForm()
-        return render(request, 'app/signup.html', locals())
+        return render(request, 'app/signup.html', {'form': form})
     
     def post(self, request):
         form = CustomerRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Registration Successful")
+            return redirect('login')
         else:
             messages.warning(request, "Invalid Input Data")
-        return render(request, 'app/signup.html', locals())
-
+        return render(request, 'app/signup.html', {'form': form})
 
 class ProfileView(View):
     def get(self, request):
         form = CustomerProfileForm()
-        return render(request, 'app/profile.html', locals())
+        return render(request, 'app/profile.html', {'form': form})
     
     def post(self, request):
         form = CustomerProfileForm(request.POST)
@@ -70,21 +64,27 @@ class ProfileView(View):
             
             reg = Customer(user=user, name=name, locality=locality, mobile=mobile, city=city, state=state, zipcode=zipcode)
             reg.save()
-            messages.success(request, "Congratulations! Profile Save Successfully")
+            messages.success(request, "Congratulations! Profile Saved Successfully")
         else:
-            messages.success(request, "Invalid Input Data")
-        return render(request, 'app/profile.html', locals())
-
+            messages.warning(request, "Invalid Input Data")
+        return render(request, 'app/profile.html', {'form': form})
 
 def address(request):
     add = Customer.objects.filter(user=request.user)
-    return render(request, 'app/address.html', locals())
+    return render(request, 'app/address.html', {'add': add})
 
-class updateAddress(View):
+class UpdateAddress(View):
     def get(self, request, pk):
-        form = CustomerProfileForm()
-        return render(request, 'app/updateAddress.html'.locals())
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
+        return render(request, 'app/updateAddress.html', {'form': form})
     
     def post(self, request, pk):
-        form = CustomerProfileForm(request.POST)
-        return render(request, 'app/address.html', locals())
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(request.POST, instance=add)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile Updated Successfully")
+        else:
+            messages.warning(request, "Invalid Input Data")
+        return redirect('address')
