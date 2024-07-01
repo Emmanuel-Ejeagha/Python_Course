bf3bd11478121951944fb9196ee1baf25eddfff5
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-01 16:20:49 +0100

    Added Payment and Orderplaced

--- Django/Ecom/ecomm/app/views.py
+++ Django/Ecom/ecomm/app/views.py
@@ -109,6 +109,17 @@ def show_cart(request):
     totalamount = amount + 1000
     return render(request, 'app/addtocart.html', locals())
 
+class checkout(View):
+    def get(self, request):
+        user = request.user
+        add = Customer.objects.filter(user=user)
+        cart_items = Cart.objects.filter(user=user)
+        famount = 0
+        for p in cart_items:
+            value = p.quantity * p.product.discounted_price
+            famount = famount + value
+        totalamount = famount + 1000
+        return render(request, 'app/checkout.html', locals())
 
 def plus_cart(request):
     if request.method == 'GET':
@@ -126,6 +137,45 @@ def plus_cart(request):
         # print(prod_id)
         data = {
             'quantity':c.quantity,
+            'amount':amount,
+            'totalamount':totalamount
+        }
+        return JsonResponse(data)
+    
+def minus_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity -= 1
+        c.save()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity * p.product.discounted_price
+            amount += value
+        totalamount = amount + 1000
+        # print(prod_id)
+        data = {
+            'quantity':c.quantity,
+            'amount':amount,
+            'totalamount':totalamount
+        }
+        return JsonResponse(data)
+    
+def remove_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.delete()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity
+            amount += value
+        totalamount = amount + 40
+        data={
             'amount':amount,
             'totalamount':totalamount
         }
