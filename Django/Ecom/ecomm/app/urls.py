27cd7e137849ba07817fd9b922d540ef6f838dc8
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-01 16:21:10 +0100

    Added Payment and Orderplaced

--- Django/Ecom/ecomm/app/urls.py
+++ Django/Ecom/ecomm/app/urls.py
@@ -18,8 +18,10 @@ urlpatterns = [
     path("updateAddress/<int:pk>", views.UpdateAddress.as_view(), name="updateAddress"),
     path("add-to-cart", views.add_to_cart, name='add-to-cart'),
     path('cart', views.show_cart, name='showcart'),
-    path('checkout/', views.show_cart, name='checkout'),
+    path('checkout/', views.checkout.as_view(), name='checkout'),
     path('pluscart/', views.plus_cart),
+    path('minuscart/', views.minus_cart),
+    path('removecart/', views.remove_cart),
     # login authentication
     path('signup/', views.CustomerRegistrationView.as_view(), name='signup'),
     path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
