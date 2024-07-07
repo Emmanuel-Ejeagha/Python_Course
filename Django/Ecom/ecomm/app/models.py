7f34bfcfe84728b2021e7c5388be19f84f22a554
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-07 23:13:45 +0100

    Modified

--- Django/Ecom/ecomm/app/models.py
+++ Django/Ecom/ecomm/app/models.py
@@ -102,9 +102,9 @@ STATUS_CHOICES = (
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.FloatField()
-    razorpay_order_id = models.CharField(max_length=100, blank=True, null=True)
-    razorpay_payment_status = models.CharField(max_length=100, blank=True, null=True)
-    razorpay_payment_id = models.CharField(max_length=100, blank=True, null=True)
+    stripe_order_id = models.CharField(max_length=100, blank=True, null=True)
+    stripe_payment_status = models.CharField(max_length=100, blank=True, null=True)
+    stripe_payment_id = models.CharField(max_length=100, blank=True, null=True)
     paid = models.BooleanField(default=False)
      
 
