9ba2b72db5e1971d56e060c05658b3eeee1b0a64
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-14 23:01:42 +0100

    Added: products endpoint

--- Django/Ecom/ecomm/app/admin.py
+++ Django/Ecom/ecomm/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from . models import Product, Customer, Cart, Payment, OrderPlaced, Wishlist
-
+from django.utils.html import format_html
+from django.contrib.auth.models import Group
+from django.urls import reverse
 # Register your models here.
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
@@ -14,6 +16,9 @@ class CustomerModelAdmin(admin.ModelAdmin):
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'product', 'quantity']
+    def products(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html(',a href="{}">{}</a>', link, obj.product.title)
     
 @admin.register(Payment)
 class PaymentModelAdmin(admin.ModelAdmin):
@@ -22,9 +27,27 @@ class PaymentModelAdmin(admin.ModelAdmin):
     
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status', 'payment']
+    list_display = ['id', 'user', 'customers', 'products', 'quantity', 'ordered_date', 'status', 'payments']
+    def customers(self, obj):
+        link = reverse("admin:app_customer_change", args=[obj.customer.pk])
+        return format_html(',a href="{}">{}</a>', link, obj.customer.name)
+
+    def products(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html(',a href="{}">{}</a>', link, obj.product.title)
+    
+    def payments(self, obj):
+        link = reverse("admin:app_payment_change", args=[obj.payment.pk])
+        return format_html(',a href="{}">{}</a>', link, obj.payment.stripe_payment_id)
+
     
     
 @admin.register(Wishlist)
 class WishlistModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'product']
+    list_display = ['id', 'user', 'products']
+    def products(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html(',a href="{}">{}</a>', link, obj.product.title)
+
+
+admin.site.unregister(Group)
